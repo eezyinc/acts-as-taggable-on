1aac4f8071350a9a89c2ac5d3b4290fcdc0837b7
Yi Lin <yi@savingsapp.com>
2013-10-10 15:55:08 -0700

    Add locale validations

--- lib/acts_as_taggable_on/tag.rb
+++ lib/acts_as_taggable_on/tag.rb
@@ -11,7 +11,8 @@ module ActsAsTaggableOn
     ### VALIDATIONS:
 
     validates_presence_of :name
-    validates_uniqueness_of :name
+    validates_presence_of :locale
+    validates_uniqueness_of :name, scope: :locale
     validates_length_of :name, :maximum => 255
 
     ### SCOPES:
